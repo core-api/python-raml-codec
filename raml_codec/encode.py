d89cce18acb15cf143e8765c91adc79f0a2a1f45
Tom Christie <tom@tomchristie.com>
2016-09-28 22:06:02 +0100

    First working pass

--- raml_codec/encode.py
+++ raml_codec/encode.py
@@ -2,91 +2,97 @@ import os
 import yaml
 
 
-def get_links(node, keys=()):
+def get_resources(node, keys=()):
     """
-    Return a dict of {keys: link} containing each link in the document.
+    Return a dict of {url: resource}.
     """
-    links = {}
+    resources = {}
     for key, link in getattr(node, 'links', {}).items():
+        # Get all the resources at this level
         index = keys + (key,)
-        links[index] = link
+        if link.url not in resources:
+            resources[link.url] = {}
+        resources[link.url][link.action] = get_resource(index, link)
     for key, data in getattr(node, 'data', {}).items():
+        # Descend into any nested structures.
         index = keys + (key,)
-        links.update(get_links(data, index))
-    return links
-
-
-def get_url_to_links_mapping(links):
-    """
-    Given a dict of {keys: link} containing each link in the document.
-    Return a dict of {url: [(keys, link), ...]}
-    """
-    urls = {}
-    for keys, link in links.items():
-        if link.url not in urls:
-            urls[link.url] = []
-        urls[link.url].append((keys, link))
-    return urls
-
-
-def get_path_components(urls):
-    """
-    Return a list of path component lists, giving a tree structure
-    for the given list of urls.
-
-    For example,
-    ['/users', '/users/{pk}', '/groups', '/groups/{pk}']
-
-    ==>
-
-    [
-        ['/users'],
-        ['/users', '/{pk}'],
-        ['/groups'],
-        ['/groups', '/{pk}'],
-    ]
-    """
-    ret = []
-    parents = ['']
-
-    for url in sorted(set(urls)):
-        while parents:
-            parent_url = ''.join(parents)
-            if url.startswith(parent_url):
-                component = url[len(parent_url):]
-                parents.append(component)
-                ret.append(parents[1:])
-                break
-            parents.pop()
-
-    return ret
+        resources.update(get_resources(data, index))
+    return resources
 
 
 def get_resource(keys, link):
     display_name = '_'.join(keys)
+    path_fields = [field for field in link.fields if field.location == 'path']
+    query_fields = [field for field in link.fields if field.location == 'query']
     return {
-        'displayName': display_name,
         'description': link.description
     }
 
 
+def insert_into(target, keys, value):
+    """
+    Nested dictionary insertion.
+
+    >>> example = {}
+    >>> insert_into(example, ['a', 'b', 'c'], 123)
+    >>> example
+    {'a': {'b': {'c': 123}}}
+    """
+    for key in keys[:-1]:
+        if key not in target:
+            target[key] = {}
+        target = target[key]
+    target[keys[-1]] = value
+
+
+def layout_resources(resources):
+    """
+    Transform a URL-dict of resources into a RAML layout of resources.
+
+    {
+        '/users': {'get': ...},
+        '/users/{pk}': {'get': ..., 'delete': ...},
+        '/groups': {'get': ...},
+    ]
+
+    ==>
+
+    {
+        '/users': {
+            'get': ...
+            '/{pk}': {
+                'get': ...
+                'delete': ...
+        '/groups': {
+            'get': ...
+        }
+    ]
+    """
+    output = {}
+    parents = ['']
+
+    items = sorted(resources.items(), key=lambda item: item[0])
+    for url, resource in items:
+        while parents:
+            parent_url = ''.join(parents)
+            if url.startswith(parent_url):
+                component = '/' + url[len(parent_url):].strip('/')
+                parents.append(component)
+                insert_into(output, parents[1:], resource)
+                break
+            parents.pop()
+
+    return output
+
+
 def encode_raml(document):
-    links = get_links(document)
-    urls = [link.url for link in links.values()]
-    urls_to_links = get_url_to_links_mapping(links)
-
-    for components in get_path_components(urls):
-        url = ''.join(components)
-        links = urls_to_links[url]
-        for keys, link in links:
-            index = ['/' + component.lstrip('/') for component in components]
-            index.append(link.action)
-            print index, get_resource(keys, link)
-
-        ##print components, [link.action for keys, link in urls_to_links[url]]
-
+    resources = get_resources(document)
+    resources = layout_resources(resources)
     structure = {
         'title': document.title,
-        'baseUri': document.url,
+        'baseUri': 'http://127.0.0.1:8000' #document.url,
     }
-    return yaml.safe_dump(structure, default_flow_style=False)
+    structure.update(resources)
+    output = '#%RAML 0.8\n'
+    output += yaml.safe_dump(structure, default_flow_style=False)
+    return output
